f2706482daa4a8410211de5804703e12463786a2
Brankho <a20213823@pucp.edu.pe>
2024-05-21 13:53:44 -0500

    PROVEEDOR CONTROLLER

--- ProyectoSuministroController/ProveedorController.cpp
+++ ProyectoSuministroController/ProveedorController.cpp
@@ -16,19 +16,18 @@ List<Proveedor^>^ ProyectoSuministroController::ProveedorController::buscarProve
 		int codigo = Convert::ToInt32(datos[0]);
 		String^ razonSocial = datos[1];
 		String^ ruc = datos[2];
-		String^ tipo = datos[3];
+		String^ calidadAgua = datos[3];
 		String^ telefono= datos[4];
 		String^ correo = datos[5];
-		String^ calidadAgua = datos[6];
+		String^ calidadInfraestructura = datos[6];
 		String^ servicioEmergencia = datos[7];
 		String^ estacion = datos[8];
-		String^ calidadinfraestructura;
 		String^ direccion;
 		String^ ciudad;
 		String^ distrito;
 
 		if (ruc->CompareTo(RUCResidencial) == 0) {
-			Proveedor^ objProveedor = gcnew Proveedor(codigo, calidadAgua, calidadinfraestructura, servicioEmergencia, estacion, ruc, direccion, telefono, razonSocial, ciudad, distrito, correo);
+			Proveedor^ objProveedor = gcnew Proveedor(codigo, calidadAgua, calidadInfraestructura, servicioEmergencia, estacion, ruc, direccion, telefono, razonSocial, ciudad, distrito, correo);
 			listaProveedores->Add(objProveedor);
 		}
 	}
